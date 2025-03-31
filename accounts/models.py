c8e32e524cf6493e3d6161a4c398ae605eeee9ec
choisieon <la-poderosa@naver.com>
2025-03-31 13:38:24 +0900

    accounts modeling/migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django_resized import ResizedImageField
 
 # Create your models here.
+class User(AbstractUser):
+    ## username
+    ## password가 이미 있는 것임
+    profile_image = ResizedImageField(
+        size = [500,500],
+        crop = ['middle', 'center'],
+        upload_to = 'profile'
+    )
