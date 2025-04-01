be00cbfa978e2c25023210c20c4dc0fb12753ea1
choisieon <la-poderosa@naver.com>
2025-04-01 12:09:48 +0900

    하트기능구현

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
-
+    form = CommentForm()
     context = {
         'posts' : posts,
+        'form' : form,
     }
     return render(request, 'index.html', context)
 
@@ -29,3 +30,28 @@ def create(request):
             'form' : form,
         }
         return render(request, 'create.html', context)
+
+
+@login_required
+def comment_create(request, post_id):
+    form = CommentForm(request.POST)
+    
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.user = request.user
+        comment.post_id = post_id 
+        comment.save()
+        return redirect('posts:index')
+    
+def like(request, post_id):
+    user = request.user
+    post = Post.objects.get(id=post_id)
+
+    #if post in user.like_posts.all()
+    if user in post.like_users.all():
+        #user.like_user.remove(post)
+        post.like_users.remove(user)
+    else:
+        #user.like_posts.add(post)
+        post.like_users.add(user)
+    return redirect('posts:index')
